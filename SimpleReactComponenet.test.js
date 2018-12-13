64e4aff059cb32cd8ca0602244a5f14330b99dea
Jeff Farr <jefarr@wayfair.com>
2018-12-13 11:32:10 -0500

    setup environment with jest, sinon, react-redux, graphql and babel

---
+++ SimpleReactComponenet.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+describe('<SimpleReactComponent />', async () => {
+  it('Executes a query using mocked data for a happy path', async () => {});
+});
---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // testRegex: '\*.test.js$',
+  reporters: ['default'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['html'],
+  collectCoverageFrom: [
+    '*.js',
+    '!.test.js'
+  ],
+  moduleNameMapper: {
+    '^@siteroot(.*)$': '<rootDir>/src/site$1',
+    '^@components(.*)$': '<rootDir>/src/site/components$1',
+    '^@hocs(.*)$': '<rootDir>/src/site/hocs$1',
+    '^@types(.*)$': '<rootDir>/src/site/types$1'
+  },
+  // setupFiles: ['./testHelpers/setupTests.js'],
+  // snapshotSerializers: ['enzyme-to-json/serializer']
+};
