a804c10bc766c4baa982856b6f788b2783c70318
Jeff Farr <jefarr@wayfair.com>
2018-12-13 13:25:32 -0500

    Demonstrated wrapper.debug() and cleaned up some comments

--- SimpleReactComponent.test.js
+++ SimpleReactComponent.test.js
@@ -12,15 +12,17 @@ describe('<SimpleReactComponent />', async () => {
     // We can use the .html() method to get a text rendering.
     expect(wrapper.html()).toBe('<h3>HELLO WORLD</h3>');
 
-    // .text() will return the inner html text
+    // .text() will return the inner html
     expect(wrapper.text()).toBe('HELLO WORLD');
 
     // .get() will retrieve the rendered elementy by index
     // We have to use toEqual here because the rendered result we get()
-    // will not be the _exact same element "in memory" as the expected definition we pass in
+    // will not be the _exact same element "in memory"_ as the expected definition we pass in
     expect(wrapper.get(0)).toEqual(<h3>HELLO WORLD</h3>);
 
     // maybe the most important method of wrapper is .debug()
-    expect(wrapper.debug()).toBe('');
+    // this is perfect for using with console.log(), not something you actually
+    // want to use in an expect()
+    expect(wrapper.debug()).toBe('<h3>\n  HELLO WORLD\n</h3>');
   });
 });
